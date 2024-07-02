2b1ac143651da26ca2050c58697d13844ea93fef
DmitriyPetunin <154313405+DmitriyPetunin@users.noreply.github.com>
2024-07-02 11:38:11 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    var navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
